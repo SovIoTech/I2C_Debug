e2e8bcf4d8f99684a6df2d7d7a880602b68cae89
SovIoTech <soviotechsolutions@gmail.com>
2025-07-07 19:00:30 +0500

    Update i2c_lib.ino

--- i2c_lib.ino
+++ i2c_lib.ino
@@ -1,31 +1,29 @@
-#include <Wire.h>
+#include <Arduino.h>
+#include <Seeed_Arduino_Relay.h>
 
-#define RELAY_I2C_ADDRESS 0x20
+Relay relay(RELAY_I2C_ADDRESS); // 0x20
 
 void setup() {
-  Wire.begin(); // Initialize I2C
   Serial.begin(115200);
-  Serial.println("4-Channel SPDT Relay Test");
-}
-
-void setRelay(uint8_t relayNum, bool state) {
-  Wire.beginTransmission(RELAY_I2C_ADDRESS);
-  Wire.write(relayNum);  // Relay number (1-4)
-  Wire.write(state ? 1 : 0);  // State (1=ON, 0=OFF)
-  Wire.endTransmission();
+  while (!Serial);
+  
+  if (!relay.begin()) {
+    Serial.println("Relay not found");
+    while(1);
+  }
+  Serial.println("Relay found");
 }
 
 void loop() {
-  // Example: Cycle through each relay
   for(int i = 1; i <= 4; i++) {
     Serial.print("Turning ON Relay ");
     Serial.println(i);
-    setRelay(i, true);
+    relay.on(i);
     delay(1000);
     
     Serial.print("Turning OFF Relay ");
     Serial.println(i);
-    setRelay(i, false);
+    relay.off(i);
     delay(1000);
   }
 }
